d86a5ed85eea97b2e019d34ae8397ed62ed2b455
KeerthiErnapu <127508885+KeerthiErnapu@users.noreply.github.com>
2025-01-05 17:29:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,13 +13,9 @@ app1.use(express.urlencoded({ extended: true }));
 app1.use(express.json());
 const { initializeApp }=require("firebase/app");
 const { getFirestore, query, where, getDocs, addDoc } = require('firebase-admin/firestore');
-try {
+
   // Get the Firebase credentials JSON from the environment variable
   const firebaseCredentials = process.env.FIREBASE_CREDENTIALS;
-
-  if (!firebaseCredentials) {
-    throw new Error('Firebase credentials environment variable not set.');
-  }
 
   // Parse the JSON string directly
   const serviceAccount = JSON.parse(firebaseCredentials);
